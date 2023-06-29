46261f6105f56646cd4d1194739d6b12c6ece6d1
pscpeng <pscpeng@outlook.com>
2023-06-30 00:15:58 +0800

    Add support for PaintedKittiDataset

--- pcdet/datasets/__init__.py
+++ pcdet/datasets/__init__.py
@@ -14,6 +14,7 @@ from .lyft.lyft_dataset import LyftDataset
 from .once.once_dataset import ONCEDataset
 from .argo2.argo2_dataset import Argo2Dataset
 from .custom.custom_dataset import CustomDataset
+from .painted_kitti.painted_kitti_dataset import PaintedKittiDataset
 
 __all__ = {
     'DatasetTemplate': DatasetTemplate,
@@ -24,7 +25,8 @@ __all__ = {
     'LyftDataset': LyftDataset,
     'ONCEDataset': ONCEDataset,
     'CustomDataset': CustomDataset,
-    'Argo2Dataset': Argo2Dataset
+    'Argo2Dataset': Argo2Dataset,
+    'PaintedKittiDataset': PaintedKittiDataset
 }
 
 
